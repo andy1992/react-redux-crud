96b8d4573c67c5a4842438865041a2fbf6455b74
Andy Wijaya <andywijaya1992@yahoo.com>
2017-01-23 03:15:40 +0700

    -    Add access control allow origin to allow request to data (in case of different port)

--- config/core.php
+++ config/core.php
@@ -2,6 +2,8 @@
 // show error reporting
 error_reporting(E_ALL);
 
+header('Access-Control-Allow-Origin: http://localhost:8080');
+
 // set your default time-zone
 date_default_timezone_set('Asia/Bangkok');
 ?>
